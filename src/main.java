70dbf8fe48b6125f25142b5e33f7f0e231cc1db4
SourcedFromWill <wp@harlowhill.com>
2018-07-20 16:23:09 +0100

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class main {
+    private JButton button1;
+    private JTextPane textPane1;
+
+    public void main(){
+        System.out.println("Hello World");
+
+        System.out.println();
+
+        System.out.println("");
+
+
+    }
+
+}
